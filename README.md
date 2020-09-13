5d0a928906ed1bd93ba87ae93c0c99b0cf037c6f
Jon Raphaelson <jon@accidental.cc>
2020-09-13 02:19:13 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# JsonRFC
+
+Pure-elixir implementations of IETF RFCs 6902 and 6902,
+JSON Pointer and Patch respectively.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `jsonrfc` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:jsonrfc, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/jsonrfc](https://hexdocs.pm/jsonrfc).
+
---
+++ mix.exs
@@ -0,0 +1,42 @@
+defmodule Json.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :jsonrfc,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/lygaret/jsonrfc"
+    ]
+  end
+
+  def application, do: []
+
+  defp deps do
+    [
+      {:espec, "~> 1.8.2", only: :test},
+      {:coverex, "~> 1.4.10", only: :test},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description, do: String.trim """
+    Implementations of JSON RFC 6901 and 6902, Pointers and Patch respectively.
+    Pointer allows evaluating and transforming a JSON document at a given keypath.
+    Patch encodes operations that abstract pointer transformations.
+  """
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/lygaret/jsonrfc",
+        "Homepage" => "https://accidental.cc"
+      }
+    ]
+  end
+
+end
