0604ef8cb84b1d97fc9f334b73965f8a20b12825
Jon Raphaelson <jon@accidental.cc>
2020-09-14 00:05:00 -0600

    release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Json.MixProject do
   def project do
     [
       app: :jsonrfc,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       description: description(),
       package: package(),
