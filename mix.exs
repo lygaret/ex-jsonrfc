cc6af70b4f9d79c2db7144c6ff879530e0f7c7c5
Jon Raphaelson <jon@accidental.cc>
2020-09-18 02:15:17 -0600

    setup ci

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule Json.MixProject do
       package: package(),
       deps: deps(),
       source_url: "https://github.com/lygaret/ex-jsonrfc",
+      dialyzer: dialyzer(),
       docs: [
         main: "readme",
         extras: ["README.md"]
@@ -23,15 +24,19 @@ defmodule Json.MixProject do
     [
       {:espec, "~> 1.8.2", only: :test},
       {:coverex, "~> 1.4.10", only: :test},
-      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.22", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
-  defp description, do: String.trim """
-    Implementations of JSON RFC 6901 and 6902, Pointers and Patch respectively.
-    Pointer allows evaluating and transforming a JSON document at a given keypath.
-    Patch encodes operations that abstract pointer transformations.
-  """
+  defp description,
+    do:
+      String.trim("""
+        Implementations of JSON RFC 6901 and 6902, Pointers and Patch respectively.
+        Pointer allows evaluating and transforming a JSON document at a given keypath.
+        Patch encodes operations that abstract pointer transformations.
+      """)
 
   defp package() do
     [
@@ -43,4 +48,10 @@ defmodule Json.MixProject do
     ]
   end
 
+  defp dialyzer() do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
 end
